7e312a7083ef4250a0a4343d2ff020fc62ec5c5e
cyberplanner <saleh.gfx@gmail.com>
2016-07-12 21:48:28 +0100

    11_saving_entry_station, tests passed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,6 +2,7 @@ class Oystercard
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
   attr_reader :balance
+  attr_reader :entry_station
 
   def initialize
     @balance = 0
@@ -12,21 +13,25 @@ class Oystercard
     @balance += amount
   end
 
-  def touch_in
+  def touch_in(station)
   raise "insufficient funds" if balance < MINIMUM_FARE
-    @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
-    @in_journey = false
     deduct(MINIMUM_FARE)
+    @entry_station = nil
   end
 
   def in_journey?
-    @in_journey
+    !!entry_station
   end
 
-  private
+  def entry_station
+    @entry_station
+  end
+
+private
 
   def deduct(amount)
     @balance -= amount
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,21 +1,25 @@
 require "oystercard"
 
 describe Oystercard do
+let(:station) { double :station }
+
+  it {is_expected.to respond_to(:entry_station) }
+
   it 'checks to see if the balance is 0' do
     expect(subject.balance).to eq 0
   end
 
   describe "#top_up" do
     it "top up the balance" do
-      expect{ subject.top_up(1) }.to change{ subject.balance }.by(1)
+      expect{ subject.top_up(Oystercard::MINIMUM_FARE) }.to change{ subject.balance }.by(Oystercard::MINIMUM_FARE)
     end
     it "raises error if exceeds the limit" do
       maximum_balance = Oystercard::MAXIMUM_BALANCE
       subject.top_up(maximum_balance)
-      expect{ subject.top_up(1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+      expect{ subject.top_up(Oystercard::MINIMUM_FARE) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
   end
-
+1
   # describe "#deduct" do
   #   it "deducts from balance" do
   #     subject.top_up(10)
@@ -30,13 +34,13 @@ describe Oystercard do
     end
 
     it 'touch in' do
-      subject.top_up(1)
-      subject.touch_in
+      subject.top_up(Oystercard::MINIMUM_FARE)
+      subject.touch_in(station)
       expect(subject).to be_in_journey
     end
     it 'touch out' do
-      subject.top_up(1)
-      subject.touch_in
+      subject.top_up(Oystercard::MINIMUM_FARE)
+      subject.touch_in(station)
       subject.touch_out
       expect(subject).not_to be_in_journey
     end
@@ -44,13 +48,19 @@ describe Oystercard do
 
     it "checks balance on touch in" do
       subject.balance < Oystercard::MINIMUM_FARE
-      expect{ subject.touch_in }.to raise_error "insufficient funds"
+      expect{ subject.touch_in(station) }.to raise_error "insufficient funds"
     end
 
     it 'charge balance on touch out' do
-      subject.top_up(10)
-      subject.touch_in
+      subject.top_up(Oystercard::MINIMUM_FARE)
+      subject.touch_in(station)
       expect{ subject.touch_out }.to change{ subject.balance }.by -Oystercard::MINIMUM_FARE
+    end
+
+    it "remembers entry station" do
+        subject.top_up(Oystercard::MINIMUM_FARE)
+        subject.touch_in(station)
+        expect(subject.entry_station).to eq station 
     end
 
 end
