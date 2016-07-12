79c4d9c31d9657f7eca05e7e3ac14fb175cc6d86
cyberplanner <saleh.gfx@gmail.com>
2016-07-12 16:23:24 +0100

    07_deducting_money, test passed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,4 +17,12 @@ describe Oystercard do
       expect{ subject.top_up(1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
   end
+
+  describe "#deduct" do
+    it { is_expected.to respond_to(:deduct).with(1).argument}
+    it "deducts from balance" do
+      subject.top_up(10)
+      expect{ subject.deduct(3) }.to change{ subject.balance }.by -3
+    end
+  end
 end
