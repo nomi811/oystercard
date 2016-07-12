4dad45042db68797700e3456c523f501e4ed0d84
cyberplanner <saleh.gfx@gmail.com>
2016-07-12 18:23:39 +0100

    09_min_balance_on_touch_in, test passed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,6 @@
 class Oystercard
   MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 1
   attr_reader :balance
 
   def initialize
@@ -16,6 +17,7 @@ class Oystercard
   end
 
   def touch_in
+  raise "insufficient funds" if balance < MINIMUM_BALANCE
     @in_journey = true
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -24,22 +24,27 @@ describe Oystercard do
   end
 
   describe 'status of card' do
-    it { is_expected.to respond_to :touch_in }
-    it { is_expected.to respond_to :touch_out}
-    it { is_expected.to respond_to :in_journey? }
 
     it 'is initially not in a journey' do
       expect(subject).not_to be_in_journey
     end
 
     it 'touch in' do
+      subject.top_up(1)
       subject.touch_in
       expect(subject).to be_in_journey
     end
     it 'touch out' do
+      subject.top_up(1)
       subject.touch_in
       subject.touch_out
       expect(subject).not_to be_in_journey
     end
   end
+
+    it "checks balance on touch in" do
+      #subject.balance < Oystercard::MINIMUM_BALANCE
+      expect{ subject.touch_in }.to raise_error "insufficient funds"
+    end
+
 end
