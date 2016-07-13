648cd65a1fd70fbdaaad1281ed678d6baa641339
Nomi Vos <nvoswork@gmail.com>
2016-07-13 08:57:59 -0500

    refractors oystercard_spec

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,8 @@
 require "oystercard"
 
 describe Oystercard do
-let(:entry_station) { double :station }
-let(:exit_station) {double :station}
+  let(:entry_station) { double :station }
+  let(:exit_station) {double :station}
 
   it {is_expected.to respond_to(:entry_station) }
 
@@ -26,18 +26,6 @@ let(:exit_station) {double :station}
     it 'is initially not in a journey' do
       expect(subject).not_to be_in_journey
     end
-
-    it 'touch in' do
-      subject.top_up(Oystercard::MINIMUM_FARE)
-      subject.touch_in(entry_station)
-      expect(subject).to be_in_journey
-    end
-    it 'touch out' do
-      subject.top_up(Oystercard::MINIMUM_FARE)
-      subject.touch_in(entry_station)
-      subject.touch_out(exit_station)
-      expect(subject).not_to be_in_journey
-    end
   end
 
     it "checks balance on touch in" do
@@ -45,30 +33,31 @@ let(:exit_station) {double :station}
       expect{ subject.touch_in(entry_station) }.to raise_error "insufficient funds"
     end
 
-    it 'charge balance on touch out' do
+  context 'card activity' do
+    before(:each) do
       subject.top_up(Oystercard::MINIMUM_FARE)
       subject.touch_in(entry_station)
+    end
+
+    it 'touch in' do
+      expect(subject).to be_in_journey
+    end
+    it 'touch out' do
+      subject.touch_out(exit_station)
+      expect(subject).not_to be_in_journey
+    end
+    it 'charge balance on touch out' do
       expect{ subject.touch_out(exit_station) }.to change{ subject.balance }.by -Oystercard::MINIMUM_FARE
     end
 
-    describe 'journey events' do
-      it "remembers entry station" do
-          subject.top_up(Oystercard::MINIMUM_FARE)
-          subject.touch_in(entry_station)
-          expect(subject.entry_station).to eq entry_station
-      end
+    it "remembers entry station" do
+        expect(subject.entry_station).to eq entry_station
     end
 
-    #
-      it 'remembers exit station' do
-        subject.top_up(Oystercard::MINIMUM_FARE)
-        subject.touch_in(entry_station)
-        subject.touch_out(exit_station)
-        expect(subject.exit_station).to eq exit_station
-      end
-      it {is_expected.to respond_to(:touch_out).with(1).argument }
-      it {is_expected.to respond_to(:exit_station) }
-    #end
-
+    it 'remembers exit station' do
+      subject.touch_out(exit_station)
+      expect(subject.exit_station).to eq exit_station
+    end
+  end
 
 end
